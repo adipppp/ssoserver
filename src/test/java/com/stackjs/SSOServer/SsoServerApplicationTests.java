d926b5620ea5c01a8e16676ffd51c4ab9480dc55
adipppp <adhif2004@gmail.com>
2024-12-23 22:19:05 +0700

    Initial commit

---
+++ src/test/java/com/stackjs/SSOServer/SsoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackjs.SSOServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
